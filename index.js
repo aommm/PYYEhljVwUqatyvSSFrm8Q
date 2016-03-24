da0208db11db465a5163922d123f8af6856f592b
Niklas Logren <niklas@niklaslogren.com>
2016-03-24 14:35:41 +0100

    changed co/promises structure; added 'succeded' counter and delete job after 10 succeeds

--- index.js
+++ index.js
@@ -1,71 +1,120 @@
 var co = require('co');
 var Promise = require("bluebird");
+var mongo = require("mongoskin");
 
 var config = require('./config');
 var utils = require('./utils');
 
+Promise.promisifyAll(mongo);
+
 function onError(err) {
   console.log('Unrecoverable error:', err);
 }
+function runMain() {
+  main().catch(onError);
+}
 
-// Wait for a job from beanstalk queue.
-// When job arrives, run it and go back to listening for new job recursively
-var getJob = co.wrap(function* () {
-  // Open new socket and connect to a tube
-  var client = utils.newBeanstalkClient();
-  yield client.watchAsync(config.bsTube);
-  console.log('waiting for job...');
-  var job = yield client.reserveAsync();
-  getJob().catch(onError); // immediately start listening for new job
-  runJob(client, job).catch(onError);
-});
+/**
+ * Launches a new worker.
+ * Worker gets a job from queue, processes it, reschedules/buries it, and starts over
+ * @returns {Promise}
+ */
+function main() {
+  return co(function*() {
+    const result = yield getJob();
+    const client = result.client;
+    const job = result.job;
+    runMain(); // immediately start listening for new job
+    console.log(job.id,"got job! processing...",job);
+    try {
+      var exchangeRate = yield getExchangeRate(job);
+    } catch (err) {}
+    if (exchangeRate)
+      yield updateMongoDb(job, exchangeRate);
+    yield maybeRescheduleJob(client, job, exchangeRate);
+    console.log(job.id,'job done');
+    client.disconnect();
+  });
+}
 
-// Run a given job
-var runJob = co.wrap(function* (client, job) {
-  job.data = JSON.parse(job.data);
-  if (typeof job.data.failed == 'undefined')
-    job.data.failed = 0;
+/**
+ * Gets a job from beanstalk queue and parses into a usable format
+ * @returns {Promise} - returns e.g. {client: ..., job: ...}
+ */
+function getJob() {
+  return co(function*() {
+    // Open new socket and connect to a tube
+    var client = utils.newBeanstalkClient();
+    yield client.watchAsync(config.bsTube);
+    // Get job
+    console.log('waiting for job...');
+    var job = yield client.reserveAsync();
+    // Handle input
+    job.data = JSON.parse(job.data);
+    if (typeof job.data.failed == 'undefined')
+      job.data.failed = 0;
+    if (typeof job.data.succeeded == 'undefined')
+      job.data.succeeded = 0;
+    return {client: client, job: job};
+  });
+}
 
-  console.log(job.id,"got job! processing...",job);
-  var success = yield processJob(job);
-  // TODO: can maybe inspect promise object instead of success?
+/**
+ * Takes a job and its result, and puts the result into mongodb
+ * @param {Object} job
+ * @param {Object} success
+ * @returns {Promise}
+ */
+function updateMongoDb(job, exchangeRate) {
+  return co(function*() {
+    console.log("updateMongoDb", arguments);
+  });
+}
 
-  //yield client.deleteJob(job.id);
-  //return;
+/**
+ * Takes a job and buries/reschedules it in beanstalk queue
+ * @param {Object} client
+ * @param {Object} job
+ * @param {Object} success
+ * @returns {Promise}
+ */
+function maybeRescheduleJob(client, job, success) {
+  return co(function*() {
+    console.log("maybeRescheduleJob", arguments);
+    if (success) {
+      job.data.succeeded++;
+    } else {
+      job.data.failed++;
+    }
+    // Failed too much? bury job
+    if (job.data.failed >= 3) {
+      console.log(job.id,'bury');
+      yield client.buryAsync(job.id);
+    }
+    // Succeeded enough times? Delete it
+    else if (job.data.succeeded >= 10) {
+      console.log(job.id,'delete');
+      yield client.deleteJobAsync(job.id);
+    }
+    else { // Else, put job back into queue
+      console.log(job.id,'put back into queue');
+      var delay = success ? 60 : 3;
+      // Delete job, then put back into queue with new body
+      yield client.deleteJobAsync(job.id);
+      var data = JSON.stringify(job.data);
+      yield client.putAsync(data, undefined, delay);
+    }
+  });
+}
 
-  if (success) {
-    // Success! Add to mongodb
-  }
-  else {
-    job.data.failed++;
-  }
-
-  // Failed too much? bury job
-  if (job.data.failed >= 3) {
-    console.log(job.id,'bury');
-    yield client.buryAsync(job.id);
-  }
-  // Else, put job back into queue
-  else {
-    console.log(job.id,'put back into queue');
-    var delay = success ? 60 : 3;
-    // Delete job, then put back into queue with new body
-    yield client.deleteJobAsync(job.id);
-    var data = JSON.stringify(job.data);
-    yield client.putAsync(data, undefined, delay);
-  }
-  console.log(job.id,'job done');
-  client.disconnect();
-});
-
-function processJob(client, job) {
+function getExchangeRate(client, job) {
   return new Promise(function(resolve, reject) {
     // doing something really expensive
     setTimeout(function() {
-      resolve(true);
+      //resolve(true);
+      reject("asdf!");
     }, 1000);
   });
 }
 
-// Run everything
-getJob().catch(onError);
+runMain();
